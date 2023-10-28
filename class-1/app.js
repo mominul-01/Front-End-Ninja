1378dba366551962f1501300d6f22ce090167695
mominul-01 <mominul.cse21@gmail.com>
2023-10-28 13:32:08 +0600

    Done first work

--- class-1/app.js
+++ class-1/app.js
@@ -2,15 +2,19 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 const parentDiv = React.createElement("div", { className: "parent" },
     [
+        React.createElement("h1", { className: "family" }, "Happy Family ❤️")
+    ],
+    
+    [
         React.createElement("div", { className: "parent-one" },
             [
-                React.createElement("h1", { className: "parent-title" }, "I am parent"),
+                React.createElement("h1", { className: "parent-title" }, "I am Head of Family 🧞‍♂️"),
                 React.createElement("p", {}, "Barir Korta")
             ]
         ),
         React.createElement("div", { className: "parent-two" },
             [
-                React.createElement("h1", { className: "parent-title" }, "I am parent two"),
+                React.createElement("h1", { className: "parent-title" }, "I am second head of Family  🧞‍♀️"),
                 React.createElement("p", {}, "Barir second Korta")
             ]
         )
